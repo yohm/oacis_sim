e87aab6e9ec91491b6355ee61ca606db18a44433
Yoshikawa Hiroyuki <yoh@yohair.local>
2019-01-30 02:40:39 +0900

    [oacis_K] modify scripts

--- K/oacis_k/BUILD.sh
+++ K/oacis_k/BUILD.sh
@@ -1,6 +1,31 @@
+#!/bin/bash
+
+# copy id_rsa.K.pub into the cwd
+if [ ! -f id_rsa.K.pub ]; then
+    read -p "specify the public key file of K: " kf
+    if [ "x${kf}" == "x" -o ! -f ${kf} ]; then
+	echo "invalid key file specified, exit."
+	exit 1
+    fi
+    cp -f ${kf} id_rsa.K.pub
+fi
+
+# export KUSER if not set
+if [ -z ${KUSER} ]; then
+    read -p "input username of K: " KUSER
+    if [ -z ${KUSER} ]; then
+	exit 1
+    fi
+fi
+
+# build docker image
 docker build -t oacis_sim/oacis_k \
        --build-arg SOCKS5_SERVER="$SOCKS5_SERVER" \
-       --build-arg SOCKS5_USER="$SOCKS5_USER" --build-arg SOCKS5_PASSWD="$SOCKS5_PASSWD" \
-       --build-arg http_proxy="$http_proxy" --build-arg https_proxy="$https_proxy" \
+       --build-arg SOCKS5_USER="$SOCKS5_USER" \
+       --build-arg SOCKS5_PASSWD="$SOCKS5_PASSWD" \
+       --build-arg http_proxy="$http_proxy" \
+       --build-arg https_proxy="$https_proxy" \
        --build-arg KUSER="$KUSER" \
        .
+
+exit $?
--- K/oacis_k/RUN.sh
+++ K/oacis_k/RUN.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-if [ -f id_rsa.K ]; then
+# invoke ssh-agen locally, and add private key of K
+_A=`ssh-add -l | grep "id_rsa.K" | wc -l`
+if [ ${_A} -lt 1 ]; then
+    kf="id_rsa.K"
+    if [ ! -f ${kf} ]; then
+	read -p "specify the private key file of K: " kf
+	if [ "x${kf}" == "x" -o ! -f ${kf} ]; then
+	    echo "invalid key file specified, exit."
+	    exit 1
+	fi
+    fi
     eval `ssh-agent`
-    ssh-add id_rsa.K
+    ssh-add ${kf}
+    if [ $? != 0 ]; then
+	exit 1
+    fi
+    echo "SSH_AUTH_SOCK=${SSH_AUTH_SOCK=}"
 fi
 
 docker run --name oacis_K --rm -p 127.0.0.1:3000:3000 -p 127.0.0.1:2222:22 -dt oacis_sim/oacis_k
