869754928060df42716ed23aa94c187d4c463f43
Yoshikawa, Hiroyuki <yoh@jp.fujitsu.com>
2019-02-03 15:45:13 +0900

    [oacis_k] modify scripts to change CRLF to LF for Windows

--- K/oacis_k/RUN.sh
+++ K/oacis_k/RUN.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+HOSTPREFX="127.0.0.1:"
+TARGHOST="localhost"
+if [[ ! -z ${DOCKER_MACHINE_NAME} ]]; then
+	HOSTPREFX=""
+	TARGHOST=`docker-machine ip`
+fi
+
 # invoke ssh-agen locally, and add private key of K
 _A=`ssh-add -l | grep "id_rsa.K" | wc -l`
 if [ ${_A} -lt 1 ]; then
@@ -20,16 +27,17 @@ if [ ${_A} -lt 1 ]; then
 fi
 
 # create/run oacis_K docker container
-docker run --name oacis_K --rm -p 127.0.0.1:3000:3000 -p 127.0.0.1:2222:22 \
+docker run --name oacis_K --rm -p ${HOSTPREFX}3000:3000 -p ${HOSTPREFX}2222:22 \
        -dt oacis_sim/oacis_k
 sleep 5
 
-# remove 'localhost' entry from knownhosts
-ssh-keygen -R localhost
-#ssh-keyscan -p 2222 -H localhost >> $HOME/.ssh/known_hosts
+# remove ${TARGHOST} entry from knownhosts
+ssh-keygen -R ${TARGHOST}
+#ssh-keyscan -p 2222 -H ${TARGHOST} >> $HOME/.ssh/known_hosts
 
 # start oacis on oacis_K via ssh with ssh-agent
-(ssh oacis@localhost -p 2222 -A /home/oacis/oacis_start.sh) &
+echo "run oacis_start.sh on ${TARGHOST}"
+(ssh oacis@${TARGHOST} -p 2222 -A /home/oacis/oacis_start.sh) &
 sleep 15
 echo
 
