8f5a31b4c0f88e57d5296c3c50f19bc4e887e127
Yoshikawa Hiroyuki <yoh@cesmba.local>
2019-02-22 11:45:00 +0900

    [PC/ffb] add GF.py

--- PC/ffb/BUILD.sh
+++ PC/ffb/BUILD.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+if [ ! -f data_tmpl.tgz ]; then
+  tar cvfz data_tmpl.tgz data tmpl
+fi
 docker build -t oacis_sim/ffb --build-arg http_proxy="$http_proxy" --build-arg https_proxy="$https_proxy" .
