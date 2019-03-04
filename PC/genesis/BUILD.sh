04ef244caedb68a5b1fbbe74b1ab59f7308c137e
Yoshikawa Hiroyuki <yoh@yohair.local>
2019-03-05 02:02:34 +0900

    [WIP][GENESIS] add prepare.sh

--- PC/genesis/BUILD.sh
+++ PC/genesis/BUILD.sh
@@ -1,1 +1,4 @@
+rm -f data.tgz
+tar cvfz data_dist.tgz data
+
 docker build -t oacis_sim/genesis --build-arg http_proxy="$http_proxy" --build-arg https_proxy="$https_proxy" .
---
+++ PC/genesis/bin/prepare.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+genesis_dir=${HOME}/genesis
+_IN="_input.json"
+if [ ! "x$1" == "x" ]; then
+  _IN=$1
+fi
+_T=`cat ${_IN} | python -m json.tool | awk -F':' '/temp_case/{print $2}' | sed -e 's/,//g' -e 's/ //g'`
+if [ "x${_T}" == "x" ]; then
+  _T=0
+fi
+_P=`cat ${_IN} | python -m json.tool | awk -F':' '/pres_case/{print $2}' | sed -e 's/,//g' -e 's/ //g'`
+if [ "x${_P}" == "x" ]; then
+  _P=0
+fi
+
+case ${_T} in
+  "1" ) _D="dataT250" ;;
+  "2" ) _D="dataT275" ;;
+  "3" ) _D="dataT300" ;;
+  "4" ) _D="dataT325" ;;
+  "5" ) _D="dataT350" ;;
+  * ) echo "invalid temp_case" 1>&2
+      exit 1 ;;
+esac
+
+case ${_P} in
+  "1" ) _DDIR="${_D}P08" ;;
+  "2" ) _DDIR="${_D}P09" ;;
+  "3" ) _DDIR="${_D}P10" ;;
+  "4" ) _DDIR="${_D}P11" ;;
+  "5" ) _DDIR="${_D}P12" ;;
+  * ) echo "invalid pres_case" 1>&2
+      exit 1 ;;
+esac
+
+rm -rf 4_equilibration
+mkdir 4_equilibration
+cp ${genesis_dir}/data/${_DDIR}/* 4_equilibration/
+cp -r ${genesis_dir}/data/1_setup ./
+cp ${genesis_dir}/data/run.inp.tmpl ./
+
+cp ${genesis_dir}/bin/*.py ./
+cp ${genesis_dir}/bin/run.sh ./
+python genesis_prep.py
+
+exit 0
+
