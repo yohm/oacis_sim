96ba9e6486ca759fff27c8200858f77a7fea562b
Yoshikawa, Hiroyuki <yoshikawa_hiroyuki@icloud.com>
2019-03-22 08:43:34 +0900

    modify BUILD.sh for ffb/genesis docker

--- PC/ffb/BUILD.sh
+++ PC/ffb/BUILD.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-if [ ! -f ffb_dist.tgz ]; then
-  tar cvfz ffb_dist.tgz bin tmpl data
-fi
 docker build -t oacis_sim/ffb --build-arg http_proxy="$http_proxy" --build-arg https_proxy="$https_proxy" .
--- PC/genesis/BUILD.sh
+++ PC/genesis/BUILD.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-if [ ! -f data_dist.tgz ]; then
-  tar cvfz data_dist.tgz data
-fi
 docker build -t oacis_sim/genesis --build-arg http_proxy="$http_proxy" --build-arg https_proxy="$https_proxy" .
